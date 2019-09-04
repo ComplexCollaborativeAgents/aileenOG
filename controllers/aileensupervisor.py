8324aafc550717e837e7464c76b80a22adb60e63
Matthew Klenk <klenk@parc.com>
2019-09-03 20:32:57 -0700

    adds the bounding boxes to the get all message

--- controllers/aileensupervisor.py
+++ controllers/aileensupervisor.py
@@ -58,10 +58,11 @@ class AileenSupervisor(Supervisor):
                 object_children = object_node.getField('children')
                 shape_node = object_children.getMFNode(0)
                 object_dict = {
-                                'id': object_node.getId(),
-                                'position': object_node.getPosition(),
-                                'orientation': object_node.getOrientation(),
-                                'bounding_object': object_node.getField('boundingObject').getSFNode().getTypeName()}
+                    'id': object_node.getId(),
+                    'position': object_node.getPosition(),
+                    'orientation': object_node.getOrientation(),
+                    'bounding_box': self.computeBoundingBox(object_node),
+                    'bounding_object': object_node.getField('boundingObject').getSFNode().getTypeName()}
                 if object_node == self._held_node:
                     object_dict['held'] = 'true'
                 else:
@@ -72,6 +73,28 @@ class AileenSupervisor(Supervisor):
         #self._supervisor.step(constants.TIME_STEP)
         return output_dict
 
+    def computeBoundingBox(self,object_node):
+        '''
+        computes the bounding box in geojson format
+        https://tools.ietf.org/html/rfc7946#section-5
+        ''' 
+        centroid = object_node.getPosition()
+        bounding_obj = object_node.getField('boundingObject').getSFNode()
+        logging.debug("[aileen_supervisor] :: computing bounding box for {}".format(
+            bounding_obj.getTypeName()))
+        if (bounding_obj.getTypeName() == "Box"):
+            size = bounding_obj.getField('size').getSFVec3f()
+            return [centroid[0]-size[0]/2, centroid[1]-size[1]/2, centroid[2]-size[2]/2,
+                    centroid[0]+size[0]/2, centroid[1]+size[1]/2, centroid[2]+size[2]/2]
+        elif (bounding_obj.getTypeName() == "Cylinder"):
+            height = bounding_obj.getField('height').getSFFloat()
+            radius = bounding_obj.getField('radius').getSFFloat()
+            return [centroid[0]-radius, centroid[1]-height/2, centroid[2]-radius,
+                    centroid[0]+radius, centroid[1]+height/2, centroid[2]+radius]
+        else:
+            raise Exception("[aileen_supervisor] :: Unable to compute bounding box for type {}".format(bounding_obj))
+
+        
     def apply_action(self, action):
         logging.debug("[aileen_supervisor] :: processing apply_action from client for action {}".format(action))
         if action is None:
