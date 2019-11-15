f1ddc7d90ffcd1f4bb65d2d0c1da23597fbeebf1
Shiwali Mohan <shiwali.mohan@parc.com>
2019-11-14 18:07:00 -0800

    completed the interaction loop for verify-evaluate

--- instructor/visual_word_lesson.py
+++ instructor/visual_word_lesson.py
@@ -24,22 +24,28 @@ class VisualWordLesson:
         self._interaction['signal'] = 'verify'
         self._interaction['content'] = LanguageGenerator.generate_language_for_object(scene_object)
 
-    def evaluate_agent_response(self):
-        pass
+    def evaluate_agent_response(self, agent_response):
+        if agent_response['status'] == 'success':
+            return {'signal': 'correct'}
 
     @staticmethod
     def administer_curriculum(world_server, agent_server):
         while True:
             raw_input("Press any key to generate the next lesson...")
 
-            lesson = VisualWordLesson().generate_lesson()
+            lesson_object = VisualWordLesson()
+            lesson = lesson_object.generate_lesson()
 
             scene_acknowledgement = world_server.set_scene(
                 {'configuration': lesson['scene'], 'label': lesson['interaction']['content']})
             logging.info("[aileen_instructor] :: received from world {}".format(scene_acknowledgement))
 
-            interaction_acknowledgement = agent_server.process_interaction(lesson['interaction'])
-            logging.info("[aileen_instructor] :: received from agent {}".format(interaction_acknowledgement))
+            agent_response = agent_server.process_interaction(lesson['interaction'])
+            logging.info("[aileen_instructor] :: received from agent {}".format(agent_response))
+
+            evaluation = lesson_object.evaluate_agent_response(agent_response)
+            agent_response = agent_server.process_interaction(evaluation)
+            logging.info("[aileen_instructor] :: provided feedback to agent")
 
 if __name__ == '__main__':
     lesson1 = VisualWordLesson()
