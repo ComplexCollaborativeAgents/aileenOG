b3a27646e0f63d2c9a46ac230f09390a29dba2d4
Aaron Ang <aaron.ang@parc.com>
2019-11-19 09:34:22 -0800

    CI: Install `sudo` if not available

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-apt-get update --fix-missing
-apt-get install -y python-setuptools build-essential
+if ! command -v sudo; then
+  apt-get update --fix-missing
+  apt-get install -y sudo
+else
+  sudo apt-get update --fix-missing
+fi
+
+sudo apt-get install -y python-setuptools build-essential
 
 (cd agent/vision; GPU=0 OPENCV=0 REBUILD=1 python2 setup.py build_ext)
 conda env create --file environment.yml
