f74a512c5d2b013b0fcddc72de9c4632a5396b66
Aaron Ang <aaron.ang@parc.com>
2019-11-25 14:09:56 -0800

    CI: Install unzip

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ else
   sudo apt-get update --fix-missing
 fi
 
-sudo apt-get install -y python-setuptools build-essential libgl1-mesa-glx
+sudo apt-get install -y python-setuptools build-essential libgl1-mesa-glx unzip
 
 (cd agent/vision; GPU=0 OPENCV=0 REBUILD=1 python2 setup.py build_ext)
 conda env create --force --file environment.yml
