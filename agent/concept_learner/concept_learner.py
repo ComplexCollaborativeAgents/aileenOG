c5b4fd28e0fb2d99597637167f06b6aa1e261764
Shiwali Mohan <shiwali.mohan@parc.com>
2019-12-04 21:59:30 -0800

    added server for concept learner

--- agent/concept_learner/concept_learner.py
+++ agent/concept_learner/concept_learner.py
@@ -1,23 +1,22 @@
-from agent.configuration import Configuration
 import subprocess
 import time
 from agent.log_config import logging
 import xmlrpclib
 import json
+import settings
 
 class ConceptLearner(object):
     def __init__(self):
         self.start_concept_learner_server()
-        self._server = xmlrpclib.ServerProxy('http://dubs:'+str(Configuration.config['ConceptLearner']['port'])+'/ConceptLearner')
+        self._server = xmlrpclib.ServerProxy('http://{}:{}/ConceptLearner'.format(settings.CONCEPT_LEARNER_HOST, settings.CONCEPT_LEARNER_PORT))
 
     def start_concept_learner_server(self):
-        cmd = 'ssh {} {}/start_concept_learner.sh {}'.format(Configuration.config['ConceptLearner']['hostname'],
-                                                             Configuration.config['ConceptLearner']['learner_path'],
-                                                             Configuration.config['ConceptLearner']['port'])
+        cmd = 'ssh {} {}/start_concept_learner.sh {}'.format(settings.CONCEPT_LEARNER_HOST,
+                                                             settings.CONCEPT_LEARNER_PATH,
+                                                             settings.CONCEPT_LEARNER_PORT)
         out = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         time.sleep(5)
-        logging.info("[concept_learner] :: started concept_learner server on {}".format(Configuration.config['ConceptLearner']
-                                                                                        ['hostname']))
+        logging.info("[concept_learner] :: started concept_learner server on {}".format(settings.CONCEPT_LEARNER_HOST))
 
     def create_new_concept(self, concept_symbol):
         response_json = self._server.create_reasoning_symbol(xmlrpclib.Binary(json.dumps({'symbol': concept_symbol})))
