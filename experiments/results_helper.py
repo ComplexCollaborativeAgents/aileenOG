7a66958945fe7b80149d90e5c6646625f321ca01
Shiwali Mohan <shiwali.mohan@parc.com>
2020-03-23 10:31:29 -0700

    module to support running of various experiments

--- experiments/results_helper.py
+++ experiments/results_helper.py
@@ -1,17 +1,37 @@
 import settings
 
+
 class ResultsHelper:
+    def __init__(self):
+        pass
+
+    do_record = True
+    gfilename = settings.RUN_DATA_FILE_PATH
+
+    @staticmethod
+    def set_do_record(state):
+        ResultsHelper.do_record = state
+
     @staticmethod
     def reset_results_file(title):
-        with open(settings.RESULTS_FILE_PATH, "w") as myfile:
-            myfile.write(title)
+        if ResultsHelper.do_record:
+            with open(ResultsHelper.gfilename, "w") as myfile:
+                myfile.write(title)
 
     @staticmethod
     def write_lesson_number_to_results_file(lesson_number):
-        with open(settings.RESULTS_FILE_PATH, "a") as myfile:
-            myfile.write("\n"+str(lesson_number)+",")
+        if ResultsHelper.do_record:
+            with open(ResultsHelper.gfilename, "a") as myfile:
+                myfile.write("\n" + str(lesson_number) + ",")
 
     @staticmethod
     def record_processing_phase(phase_string):
-        with open(settings.RESULTS_FILE_PATH, "a") as myfile:
-            myfile.write(phase_string+",")
+        if ResultsHelper.do_record:
+            with open(ResultsHelper.gfilename, "a") as myfile:
+                myfile.write(phase_string + ",")
+
+    @staticmethod
+    def record_generality_performance_score(score):
+        if ResultsHelper.do_record:
+            with open(ResultsHelper.gfilename, "a") as myfile:
+                myfile.write(str(score))
--- experiments/runner.py
+++ experiments/runner.py
@@ -1,5 +1,66 @@
-class Runner:
-    def __init__(self):
-        pass
-_
+from instructor.curriculum_on_rails import CurriculumOnRails
+from results_helper import ResultsHelper
+from log_config import logging
+import xmlrpclib
+import settings
+from instructor.curriculum import Curriculum
+
+
+def create_connection_with_aileen_world():
+    url = 'http://{}:{}'.format(settings.WORLD_HOST, settings.WORLD_PORT)
+    server = xmlrpclib.ServerProxy(url)
+    logging.info("[aileen_instructor] :: created a connection with the world server at: {}".format(url))
+    return server
+
+
+def create_connection_with_aileen_agent():
+    url = 'http://{}:{}'.format(settings.AGENT_HOST, settings.AGENT_PORT)
+    server = xmlrpclib.ServerProxy(url)
+    logging.info("[aileen_instructor] :: created a connection with the agent: {}".format(url))
+    return server
+
+
+if __name__ == '__main__':
+    settings.DO_RECORD = True
+    ResultsHelper.reset_results_file('visual-word-learning-run1')
+    lesson_number = 0
+
+    world = create_connection_with_aileen_world()
+    agent = create_connection_with_aileen_agent()
+
+    rails = CurriculumOnRails("visual-word")
+    lessons = rails.generate_inform_training_gamut()
+    exams = rails.generate_verify_testing_gamut()[0:5]
+
+    for lesson in Curriculum(lessons):
+        ResultsHelper.write_lesson_number_to_results_file(lesson_number)
+
+        lesson_object = lesson['object']
+
+        scene_acknowledgement = world.set_scene(
+            {'configuration': lesson['scene'], 'label': lesson['interaction']['content']})
+
+        agent_response = agent.process_interaction(lesson['interaction'])
+
+        evaluation = lesson_object.evaluate_agent_response(agent_response)
+        agent_response = agent.process_interaction(evaluation)
+        lesson_number = lesson_number + 1
+
+        score = 0
+        for exam in Curriculum(exams):
+            exam_object = exam['object']
+            scene_acknowledgement = world.set_scene({'configuration': exam['scene'], 'label': exam['interaction']['content']})
+            agent_response = agent.process_interaction(exam['interaction'])
+
+            if agent_response['status'] == 'success':
+                score = score + 1
+
+            evaluation = exam_object.evaluate_agent_response(agent_response)
+            agent_response = agent.process_interaction(evaluation)
+
+        ResultsHelper.record_generality_performance_score(score)
+
+
+
+
 
