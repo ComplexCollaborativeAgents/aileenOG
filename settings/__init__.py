03046ae7d3e743b5223f2c0cff9b4198b98c2d06
Shiwali Mohan <shiwali.mohan@parc.com>
2020-04-24 12:32:03 -0700

    added an agent parameter to control if the agent recomprehends after learning.

--- settings/__init__.py
+++ settings/__init__.py
@@ -86,6 +86,8 @@ AGENT_PRELOAD_SPATIAL_CONCEPTS_PARAM = 'false'
 AGENT_ACTION_CONCEPTS_PARAM = 'external'
 AGENT_PRELOAD_ACTION_CONCEPTS_PARAM = 'false'
 
+AGENT_RECOMPREHEND_AFTER_LEARN = 'false'
+
 
 ## Experiments
 RUN_DATA_FILE_PATH = path.join(ROOT_PATH, 'experiments', 'results', 'run.csv')
