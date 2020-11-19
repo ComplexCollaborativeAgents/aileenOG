e12363db0c8a98eb24f0d70d7bfcc5645b5af186
Shiwali Mohan <shiwali.mohan@parc.com>
2020-11-18 20:17:14 -0800

    corrected experiment runningcommand

--- experiments/experiment/batch_runner.py
+++ experiments/experiment/batch_runner.py
@@ -39,34 +39,35 @@ if __name__ == "__main__":
         AGENT_CMD = "python agent --port {}".format(agent_port)
         #Redefine Runner CMD
         RUNNER_CMD = 'python experiments/experiment/runner.py --file experiments/results/{}/{}-run-{}.csv --type {} ' \
-                     '--concept {} --distractors {} --episodes {} --agent_port {}'.format(experiment_name,
+                     '--concept {} --distractors {} --episodes {} --exam_length {} --agent_port {}'.format(experiment_name,
                                                                           settings.BATCH_TYPE, run,
                                                                           settings.BATCH_TYPE,
                                                                           settings.BATCH_CONCEPT,
                                                                           settings.BATCH_DISTRACTORS,
                                                                           settings.BATCH_EPISODE,
+                                                                          settings.BATCH_EXAM_LENGTH,
                                                                           agent_port)
         logging.info('[batch_runner] :: Starting run {} of {} of type {}'.format(run+1, settings.BATCH_SIZE, settings.BATCH_TYPE))
         #Run Webots
         logging.info('[batch_runner] :: Starting Webots')
-        subprocess.Popen(WEBOTS_CMD, stdout = open("experiments/results/{}/system_logs/webots-run-{}.out".format(experiment_name, run),'w'), stderr = subprocess.STDOUT, shell = True)
+        webots_proc = subprocess.Popen(WEBOTS_CMD, stdout = open("experiments/results/{}/system_logs/webots-run-{}.out".format(experiment_name, run),'w'), stderr = subprocess.STDOUT, shell = True)
         time.sleep(5)
         logging.info('[batch_runner] :: Webots Started')
         #Run World
         logging.info('[batch_runner] :: Starting World')
-        subprocess.Popen(WORLD_CMD, stdout = open("experiments/results/{}/system_logs/world-run-{}.out".format(experiment_name, run), 'w'), stderr = subprocess.STDOUT, shell = True)
+        world_proc = subprocess.Popen(WORLD_CMD, stdout = open("experiments/results/{}/system_logs/world-run-{}.out".format(experiment_name, run), 'w'), stderr = subprocess.STDOUT, shell = True)
         time.sleep(20)
         logging.info('[batch_runner] :: World Started')
         #Run Agent
         logging.info('[batch_runner] :: Starting Agent')
-        subprocess.Popen(AGENT_CMD, stdout = open("experiments/results/{}/system_logs/agent-run-{}.out".format(experiment_name, run), 'w'), stderr = subprocess.STDOUT, shell = True)
+        agent_proc = subprocess.Popen(AGENT_CMD, stdout = open("experiments/results/{}/system_logs/agent-run-{}.out".format(experiment_name, run), 'w'), stderr = subprocess.STDOUT, shell = True)
         time.sleep(25)
         logging.info('[batch_runner] :: Agent Started')
         #Run Runner
         logging.info('[batch_runner] :: Starting Runner')
         #print cmd
         logging.info("[batch_runner] :: Running experiment {}".format(RUNNER_CMD))
-        subprocess.call(RUNNER_CMD, stdout = open("experiments/results/{}/system_logs/runner-run-{}.out".format(experiment_name, run), 'w'), stderr = subprocess.STDOUT, shell = True)
+        experiment_proc = subprocess.call(RUNNER_CMD, stdout = open("experiments/results/{}/system_logs/runner-run-{}.out".format(experiment_name, run), 'w'), stderr = subprocess.STDOUT, shell = True)
         logging.info('[batch_runner] :: Runner returned')
         time.sleep(5)
 
@@ -77,6 +78,13 @@ if __name__ == "__main__":
         SCP_COMMAND = "scp dubs:aileen-agent/agent/concept_learner/concept.log experiments/results/{}/concept_logs/concept-run-{}.log".format(experiment_name, run)
         logging.info("[batch_runner] :: copying log file to {}".format(SCP_COMMAND))
         subprocess.Popen(SCP_COMMAND,shell = True)
-        subprocess.Popen("kill $(ps -u $USERNAME | grep python | awk '{print $1}')", shell = True)
-        subprocess.Popen("kill $(ps -u $USERNAME | grep webots | awk '{print $1}')", shell = True)
+
+        webots_proc.kill()
+        world_proc.kill()
+        agent_proc.kill()
+        experiment_proc.kill()
+
+        #subprocess.Popen("kill $(ps -u $USERNAME | grep python | awk '{print $1}')", shell = True)
+        #subprocess.Popen("kill $(ps -u $USERNAME | grep webots | awk '{print $1}')", shell = True)
         logging.info('[batch_runner] :: Subprocesses murdered')
+        time.sleep(15)
