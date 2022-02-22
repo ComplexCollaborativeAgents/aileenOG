712f8e0e5c01c4e44dd4b7833edd0a419a91d954
Will Hancock <whancock@parc.com>
2022-02-21 21:05:42 -0600

    updating install procedures

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,46 +11,48 @@ else
 fi
 
 $SUDO apt-get update --fix-missing
-
-$SUDO apt-get install -y python-setuptools build-essential libgl1-mesa-glx unzip
+$SUDO apt-get install -y qhull-bin python-setuptools build-essential libgl1-mesa-glx unzip
 
 (cd agent/vision; GPU=0 OPENCV=0 REBUILD=1 python2 setup.py build_ext)
-conda env create --force --file environment.yml
 
+conda env create --force --file environment.yml
 AILEEN_ENV="$(conda config --show envs_dirs | grep -o "/.*" | head -1)/aileen"
 
-###############################################################################
-# Commented out because this is already pre-installed in Docker image for CI. #
-###############################################################################
-#  
-# (
-#   cd /tmp;
-#   wget http://soar.eecs.umich.edu/downloads/SoarSuite/SoarSuite_9.6.0-Multiplatform_64bit.zip --no-check-certificate;
-#   $SUDO unzip -d /usr/local SoarSuite_9.6.0-Multiplatform_64bit.zip
-# )
-# 
-# (
-#   cd /tmp;
-#   wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.8.tar.gz;
-#   tar xf openfst-1.6.8.tar.gz;
-#   cd openfst-1.6.8;
-#   ./configure --enable-grm --prefix="$AILEEN_ENV";
-#   make -j4 install
-# )
+(
+  cd /tmp;
+  wget https://github.com/cyberbotics/webots/releases/download/R2021a/webots_2021a_amd64.deb
+  $SUDO apt install -y /tmp/webots_2021a_amd64.deb
+)
 
-# (
-#   cd /tmp;
-#   git clone https://github.com/google/re2;
-#   cd re2;
-#   git checkout 2018-04-01;
-#   make -j4 install prefix="" DESTDIR="$AILEEN_ENV"
-# )
+ 
+(
+  cd /tmp;
+  wget http://soar.eecs.umich.edu/downloads/SoarSuite/SoarSuite_9.6.0-Multiplatform_64bit.zip --no-check-certificate;
+  $SUDO unzip -d /usr/local SoarSuite_9.6.0-Multiplatform_64bit.zip
+)
 
-# (
-#   cd /tmp;
-#   wget http://www.openfst.org/twiki/pub/GRM/PyniniDownload/pynini-1.9.3.tar.gz;
-#   tar xf pynini-1.9.3.tar.gz;
-#   cd pynini-1.9.3;
-#   source activate aileen;
-#   CPATH="$AILEEN_ENV/include" python setup.py install &> /dev/null
-# )
+(
+  cd /tmp;
+  wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.8.tar.gz;
+  tar xf openfst-1.6.8.tar.gz;
+  cd openfst-1.6.8;
+  ./configure --enable-grm --prefix="$AILEEN_ENV";
+  make -j4 install
+)
+
+(
+  cd /tmp;
+  git clone https://github.com/google/re2;
+  cd re2;
+  git checkout 2018-04-01;
+  make -j4 install prefix="" DESTDIR="$AILEEN_ENV"
+)
+
+(
+  cd /tmp;
+  wget http://www.openfst.org/twiki/pub/GRM/PyniniDownload/pynini-1.9.3.tar.gz;
+  tar xf pynini-1.9.3.tar.gz;
+  cd pynini-1.9.3;
+  source activate aileen;
+  CPATH="$AILEEN_ENV/include" python setup.py install &> /dev/null
+)
