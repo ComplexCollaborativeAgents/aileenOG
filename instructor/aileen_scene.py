c68e2a46de56226170e8e31efbf0a72a66483112
Michael Fan <mfan@parc.com>
2022-03-14 17:07:34 -0700

    Update aileen_scene.py

--- instructor/aileen_scene.py
+++ instructor/aileen_scene.py
@@ -190,15 +190,15 @@ class AileenScene:
     class Randomizer:
 
         def get_random_position_on_table(self):
-            # position = [uniform(settings.OBJECT_POSITION_MIN_X, settings.OBJECT_POSITION_MAX_X),
-            #             uniform(settings.OBJECT_POSITION_MIN_Y, settings.OBJECT_POSITION_MAX_Y),
-            #             uniform(settings.OBJECT_POSITION_MIN_Z, settings.OBJECT_POSITION_MAX_Z)]
+            position = [uniform(settings.OBJECT_POSITION_MIN_X, settings.OBJECT_POSITION_MAX_X),
+                        uniform(settings.OBJECT_POSITION_MIN_Y, settings.OBJECT_POSITION_MAX_Y),
+                        uniform(settings.OBJECT_POSITION_MIN_Z, settings.OBJECT_POSITION_MAX_Z)]
             # position = [uniform(settings.OBJECT_POSITION_MAX_X / 4.0, settings.OBJECT_POSITION_MAX_X),
             #             uniform(settings.OBJECT_POSITION_MAX_Y / 4.0, settings.OBJECT_POSITION_MAX_Y),
             #             uniform(settings.OBJECT_POSITION_MAX_Z / 4.0, settings.OBJECT_POSITION_MAX_Z)]
-            position = [settings.OBJECT_POSITION_MAX_X / 2.0, settings.OBJECT_POSITION_MAX_X / 2.0,
-                        settings.OBJECT_POSITION_MAX_Y / 4.0, settings.OBJECT_POSITION_MAX_Y / 4.0,
-                        settings.OBJECT_POSITION_MAX_Z / 2.0, settings.OBJECT_POSITION_MAX_Z / 2.0]
+            # position = [settings.OBJECT_POSITION_MAX_X / 2.0, settings.OBJECT_POSITION_MAX_X / 2.0,
+            #             settings.OBJECT_POSITION_MAX_Y / 4.0, settings.OBJECT_POSITION_MAX_Y / 4.0,
+            #             settings.OBJECT_POSITION_MAX_Z / 2.0, settings.OBJECT_POSITION_MAX_Z / 2.0]
 
             # table = box(settings.OBJECT_POSITION_MIN_X,
             #         settings.OBJECT_POSITION_MIN_Z,
