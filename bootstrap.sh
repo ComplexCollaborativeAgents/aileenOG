bac4d336484d32bc5c3cf9ed6f0f43e53292f7fe
Will Hancock <wwhancock@u.northwestern.edu>
2022-12-14 15:46:08 -0600

    updating install instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ set -o pipefail
 if [ -v GITLAB_CI ]; then
   SUDO=""
 else
-  SUDO="sudo "
+SUDO="sudo "
 fi
 
 $SUDO apt-get update --fix-missing
@@ -15,8 +15,8 @@ $SUDO apt-get install -y qhull-bin python-setuptools build-essential libgl1-mesa
 
 (cd agent/vision; GPU=0 OPENCV=0 REBUILD=1 python2 setup.py build_ext)
 
-conda env create --force --file environment.yml
 AILEEN_ENV="$(conda config --show envs_dirs | grep -o "/.*" | head -1)/aileen"
+# echo "$AILEEN_ENV"
 
 (
   cd /tmp;
@@ -32,6 +32,7 @@ AILEEN_ENV="$(conda config --show envs_dirs | grep -o "/.*" | head -1)/aileen"
   pkill java
 )
 
+
 (
   cd /tmp;
   wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.8.tar.gz;
@@ -43,9 +44,11 @@ AILEEN_ENV="$(conda config --show envs_dirs | grep -o "/.*" | head -1)/aileen"
 
 (
   cd /tmp;
-  git clone https://github.com/google/re2;
-  cd re2;
-  git checkout 2018-04-01;
+  # git clone https://github.com/google/re2;
+  wget https://github.com/google/re2/archive/refs/tags/2018-04-01.tar.gz;
+  tar xf 2018-04-01.tar.gz;
+  cd re2-2018-04-01;
+  # git checkout 2018-04-01;
   make -j4 install prefix="" DESTDIR="$AILEEN_ENV"
 )
 
@@ -54,6 +57,5 @@ AILEEN_ENV="$(conda config --show envs_dirs | grep -o "/.*" | head -1)/aileen"
   wget http://www.openfst.org/twiki/pub/GRM/PyniniDownload/pynini-1.9.3.tar.gz;
   tar xf pynini-1.9.3.tar.gz;
   cd pynini-1.9.3;
-  source activate aileen;
-  CPATH="$AILEEN_ENV/include" python setup.py install &> /dev/null
+  CPATH="$AILEEN_ENV/include" python setup.py install
 )
